28b40c19f75a143378aa4e5581fa3e3696259e9c
Pam Hamrick <leahamr@yahoo.com>
2020-08-09 17:25:54 -0500

    Add code to display Mars hemispheres

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MissionToMars
 Web Scraping Project
+
+## Screenshots from the Mars Portfolio
+![](Mars_Portfolio_screenshot1.PNG)
+![](Mars_Portfolio_screenshot2.PNG)
+![](Mars_Portfolio_screenshot3.PNG)
+![](Mars_Portfolio_screenshot4.PNG)
+![](Mars_Portfolio_screenshot5.PNG)
+![](Mars_Portfolio_screenshot6.PNG)
--- scraping.py
+++ scraping.py
@@ -16,16 +16,18 @@ def scrape_all():
     
     # Use the mars_news function to pull data
     news_title, news_paragraph = mars_news(browser)
-    image1_title = hemisphere1_title(browser)
+    #   cerberus, schiaparelli, syrtis_major, valles_marineris = get_hemispheres(browser)
     # Run all the scraping functions and store results in a dictionary
     data = {"news_title": news_title,
             "news_paragraph": news_paragraph,
             "featured_image": featured_image(browser),
             "facts": mars_facts(),
             "last_modified": dt.datetime.now(),
-            "hemisphere_one": hemisphere1_img(browser),
-            "image_one_title": image1_title
-    }
+            "cerberus" : get_cerberus(browser),
+            "schiaparelli": get_schiaparelli(browser),
+            "syrtis_major": get_syrtis_major(browser),
+            "valles_marineris": get_valles_marineris(browser)
+        }
     #Stop the webdriver and return data
     browser.quit()
     return data
@@ -52,6 +54,7 @@ def mars_news(browser):
         return None, None
     
     return news_title, news_p
+
 # Original code would not work
 # def featured_image(browser):
 #     # ### Scrape Featured Image from JPL site
@@ -123,64 +126,129 @@ def mars_facts():
     #Convert dataframe to usable HTML
     return df.to_html()
 
-def  hemisphere1_img(browser):
+def get_cerberus(browser):
     # ### Scrape Featured Image from USGS site
     ### Get Mars Hemisphere images
 
     # Visit URL: USGS
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
-    html= browser.html
 
-    # Find the full image 
-    imgs_soup = BeautifulSoup(html, 'html.parser')
-    collapsible_results = imgs_soup.find('div', class_ = 'collapsible results')
-    item_one = collapsible_results.find('div', class_ = 'item')('a')[1]['href']
- 
-    # Use the base URL to create an absolute URL
-    item_one_url = f'https://astrogeology.usgs.gov{item_one}'
-
-    # Go to the link and parse html 
-    browser.visit(item_one_url)
-    html= browser.html
-    img1_soup = BeautifulSoup(html, 'html.parser')
+    hrefs = []
+    titles = []
+    for x in [0,1,2,3]:
+        # initial page click
+        browser.find_by_css('a.product-item h3')[x].click()
+        # get html from new page
+        html= browser.html
+        imgs_soup = BeautifulSoup(html, 'html.parser')
+        # get title
+        title_elem = imgs_soup.find('h2', class_ ='title').text
+        titles.append(title_elem)
+        # get href
+        href = imgs_soup.find('a', text = 'Sample').get('href')
+        hrefs.append(href)
+        browser.back()
     
-    try: 
-         # Find the relative image url
-        img1_full_url = img1_soup.select_one('ul li a').get('href')
-    except AttributeError:
-        return None
-    
-    img1_full_url = f'{img1_full_url}'
+    cerberus = hrefs[0]
+    schiaparelli = hrefs[1]
+    syrtis_major = hrefs[2]
+    valles_marineris = hrefs[3]
 
-    return img1_full_url
+    return cerberus
 
-    
-def hemisphere1_title(browser):
+def get_schiaparelli(browser):
     # ### Scrape Featured Image from USGS site
     ### Get Mars Hemisphere images
 
     # Visit URL: USGS
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
-    html= browser.html
 
-    # Find   the full image button 
-    imgs_soup = BeautifulSoup(html, 'html.parser')
-    collapsible_results = imgs_soup.find('div', class_ = 'collapsible results')
-    item_one = collapsible_results.find('div', class_ = 'item')('a')[1]['href']
- 
-    # Use the base URL to create an absolute URL
-    item_one_url = f'https://astrogeology.usgs.gov{item_one}'
-    item_one_url
+    hrefs = []
+    titles = []
+    for x in [0,1,2,3]:
+        # initial page click
+        browser.find_by_css('a.product-item h3')[x].click()
+        # get html from new page
+        html= browser.html
+        imgs_soup = BeautifulSoup(html, 'html.parser')
+        # get title
+        title_elem = imgs_soup.find('h2', class_ ='title').text
+        titles.append(title_elem)
+        # get href
+        href = imgs_soup.find('a', text = 'Sample').get('href')
+        hrefs.append(href)
+        browser.back()
+    
+    cerberus = hrefs[0]
+    schiaparelli = hrefs[1]
+    syrtis_major = hrefs[2]
+    valles_marineris = hrefs[3]
 
-    # Go to the link and parse html 
-    browser.visit(item_one_url)
-    html= browser.html
-    img1_soup = BeautifulSoup(html, 'html.parser')
+    return schiaparelli
 
-    image1_title = img1_soup.select('div', class_ = 'content')[0]('h2')[0].text
-    return image1_title
+def get_syrtis_major(browser):
+    # ### Scrape Featured Image from USGS site
+    ### Get Mars Hemisphere images
+
+    # Visit URL: USGS
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    hrefs = []
+    titles = []
+    for x in [0,1,2,3]:
+        # initial page click
+        browser.find_by_css('a.product-item h3')[x].click()
+        # get html from new page
+        html= browser.html
+        imgs_soup = BeautifulSoup(html, 'html.parser')
+        # get title
+        title_elem = imgs_soup.find('h2', class_ ='title').text
+        titles.append(title_elem)
+        # get href
+        href = imgs_soup.find('a', text = 'Sample').get('href')
+        hrefs.append(href)
+        browser.back()
+    
+    cerberus = hrefs[0]
+    schiaparelli = hrefs[1]
+    syrtis_major = hrefs[2]
+    valles_marineris = hrefs[3]
+
+    return syrtis_major
+
+def get_valles_marineris(browser):
+    # ### Scrape Featured Image from USGS site
+    ### Get Mars Hemisphere images
+
+    # Visit URL: USGS
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    hrefs = []
+    titles = []
+    for x in [0,1,2,3]:
+        # initial page click
+        browser.find_by_css('a.product-item h3')[x].click()
+        # get html from new page
+        html= browser.html
+        imgs_soup = BeautifulSoup(html, 'html.parser')
+        # get title
+        title_elem = imgs_soup.find('h2', class_ ='title').text
+        titles.append(title_elem)
+        # get href
+        href = imgs_soup.find('a', text = 'Sample').get('href')
+        hrefs.append(href)
+        browser.back()
+    
+    cerberus = hrefs[0]
+    schiaparelli = hrefs[1]
+    syrtis_major = hrefs[2]
+    valles_marineris = hrefs[3]
+
+    return valles_marineris
 
 if __name__ == "__main__":
     # If running as script, print scraped data 
